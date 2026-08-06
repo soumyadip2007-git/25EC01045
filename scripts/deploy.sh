f7a8bafce3a6858377d32dfd27f254cf79ff120b
soumyadip2007-git <soumyadipmallick276@gmail.com>
2026-08-06 18:46:41 +0000

    This repo is for my Lab Work of IEDA Course

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env tcsh
+setenv name "Soumyadip"
+echo "Grand Welcome $name"
+mkdir demo 
+cd demo
+touch file.txt
+setenv my_string "Soumyadip"
+echo "$my_string"
---
+++ scripts/setup_run.sh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+setenv RUN_NAME "run_25EC01045"
+mkdir -p ../tcad_sims/$RUN_NAME
+touch ../tcad_sims/$RUN_NAME/notes.txt
+echo "Created run folder : `cd ../tcad_sims/$RUN_NAME && pwd` "
