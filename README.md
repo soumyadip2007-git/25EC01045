c774be780dbb3e091e8b5ae708cddfc12585dee9
soumyadip2007-git <soumyadipmallick276@gmail.com>
2026-08-06 19:30:12 +0000

    Change has been made for 1st time in setup_check print statement

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 25EC01045
 Lab work for IEDA Tools course
+
+* 49bb462 (HEAD -> main, origin/main, origin/HEAD) The gitignore file has been modified
+*   22f453a Because I need it Merge branch 'master'
+|\
+| * f7a8baf (origin/master, master) This repo is for my Lab Work of IEDA Course
+* 3ef6f00 Initial commit
---
+++ check_setup.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+print("Python :",sys.version.split()[0])
+print("numpy :",np.__version__)
+print("pandas :",pd.__version__)
+print("matplotlib :",matplotlib.__version__)
+
+plt.plot([0,1,2,3],[0,1,4,9],marker = "o")
+plt.title("If you can see this window, the setup works")
+plt.xlabel("x")
+plt.ylabel("x squared")
+plt.show()
